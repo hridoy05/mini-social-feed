59e8d6e90655de653478a15fa660a356d7a6563d
hridoy05 <hridoy.banik71@gmail.com>
2026-07-17 23:40:22 +0600

    chore(mobile): initial expo router project setup

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { SymbolView } from 'expo-symbols';
+import { Tabs } from 'expo-router';
+
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <SymbolView
+              name={{
+                ios: 'house.fill',
+                android: 'home',
+                web: 'home',
+              }}
+              tintColor={color}
+              size={28}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
